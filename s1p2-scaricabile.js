12a2b4e6cd3b5fdd53d08a38cc0127a676605e9b
Francesca993 <montinifrancesca993@gmail.com>
2024-02-11 19:01:26 +0100

    first commit

---
+++ s1p2-scaricabile.js
@@ -0,0 +1,75 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// I datatypes in javascript sono: "numbers" ovverso numeri, "string" rachiuso dentro " " viene letto come testo, "Boolean" sarebbero de vero o falso,
+"undefined" o "null" ovvero un cintenuto non definito o semplicemente vuoto, "bigInt" e "symbol", poi ci sono gli "objects" ovvero coppie chiave valore e gli "array" ovvero
+ una specie di contenitore che raggruppa più 
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+un insieme di coppie chiave valore
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+let numeroUno = 12;
+let numeroDue = 20;
+let somma = numeroUno + numeroDue;
+console.log(somma);
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12;
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let name = "Francesca"
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let numeroQuattro = 4n;
+let x = 12n;
+let sottrazione = (numeroQuattro - x);
+console.log(sottrazione)
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+let name1 = "john";
+let name2 = "John";
+console.log(name1 !== name2);
+
+console.log(name1 === name2.toLowerCase);
---
+++ script.js
@@ -0,0 +1,15 @@
+let numeroUno = 12;
+let numeroDue = 20;
+let somma = numeroUno + numeroDue;
+console.log(somma);
+
+let numeroQuattro = 4n;
+let x = 12n;
+let sottrazione = numeroQuattro - x;
+console.log(sottrazione);
+
+let name1 = "john";
+let name2 = "John";
+console.log(name1 !== name2);
+
+console.log(name1 === name2.toLowerCase);
